ea0082c615c9b1d44691a415436e562027163e58
damar <damar@gitlab.lab>
2023-04-04 12:54:47 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 @Component({
   selector: 'nav-app-root',
   templateUrl: './app.component.html',
@@ -9,10 +9,27 @@ export class AppComponent implements OnInit {
   title = 'nav-app';
   classActive = 'active';
   location = '/';
+  constructor(router: Router) {
+    router.events.subscribe((event: any) => {
+      if (event instanceof NavigationStart) {
+        // Show loading indicator
+      }
+
+      if (event instanceof NavigationEnd) {
+        // Hide loading indicator
+        this.location = location.pathname
+      }
+
+      if (event instanceof NavigationError) {
+        // Hide loading indicator
+        // Present error to user
+        console.log(event.error);
+      }
+    });
+  }
 
   ngOnInit() {
     console.log('init')
     console.log(location.pathname)
-    this.location = location.pathname
   }
 }
